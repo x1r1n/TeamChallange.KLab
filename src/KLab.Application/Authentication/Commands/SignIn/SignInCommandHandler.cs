2354e219c8d10b543efb027161773d49f62a1e24
Amin Khashemi <hashemiamin666@gmail.com>
2024-03-29 13:35:25 +0200

    Cleanuped code

--- src/KLab.Application/Authentication/Commands/SignIn/SignInCommandHandler.cs
+++ src/KLab.Application/Authentication/Commands/SignIn/SignInCommandHandler.cs
@@ -2,6 +2,7 @@
 using KLab.Application.Core.Abstractions.Emails;
 using KLab.Application.Core.Abstractions.Messaging;
 using KLab.Domain.Core.Constants.Emails;
+using KLab.Domain.Core.Errors;
 using KLab.Domain.Core.Primitives;
 using KLab.Domain.Core.Primitives.ResultModel;
 
