2364f22a3c6944b5ed0c812f29a3a19e05bf1290
Amin Khashemi <hashemiamin666@gmail.com>
2024-03-29 13:07:15 +0200

    Changed ApplicationUser entity

--- src/KLab.Infrastructure/Migrations/20240327152155_ChangedAvatarUrlToImageLocalPath.Designer.cs
+++ src/KLab.Infrastructure/Migrations/20240327152155_ChangedAvatarUrlToImageLocalPath.Designer.cs
@@ -4,6 +4,7 @@ using KLab.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KLab.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240327152155_ChangedAvatarUrlToImageLocalPath")]
+    partial class ChangedAvatarUrlToImageLocalPath
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -43,6 +46,9 @@ namespace KLab.Infrastructure.Migrations
 
                     b.Property<bool>("EmailConfirmed")
                         .HasColumnType("bit");
+
+                    b.Property<string>("ImageLocalPath")
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<bool>("LockoutEnabled")
                         .HasColumnType("bit");
---
+++ src/KLab.Infrastructure/Migrations/20240329105925_ChangedApplicationUser_DeleteProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KLab.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedApplicationUser_DeleteProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLocalPath",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLocalPath",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
