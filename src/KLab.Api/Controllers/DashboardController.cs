e9bde5eb8e80a3cbbb0b1de0072e9770937219b1
Amin Khashemi <hashemiamin666@gmail.com>
2024-04-26 06:28:59 +0300

    Updated documentation for Users and Authentication controllers and sign out command handler

--- src/KLab.Api/Controllers/DashboardController.cs
+++ src/KLab.Api/Controllers/DashboardController.cs
@@ -27,11 +27,11 @@ namespace KLab.Api.Controllers
 		/// </summary>
 		/// <param name="id">The id of the user to whom the role will be assigned</param>
 		/// <param name="request">The request containing the new role name</param>
-		/// <response code="204">No Content: If the role assignment is successful</response>
-		/// <response code="400">Bad Request: If the request is invalid</response>
-		/// <response code="404">Not Found: If the user with the specified id is not found</response>
-		/// <response code="422">Unprocessable Entity: If the role assignment process encounters validation errors</response>
-		/// <response code="500">Internal Server Error: If an unexpected error occurs during processing</response>
+		/// <response code="204">If the role assignment is successful</response>
+		/// <response code="400">If the request is invalid or malformed</response>
+		/// <response code="404">If the user with the specified id is not found</response>
+		/// <response code="422">If the role assignment process encounters validation errors</response>
+		/// <response code="500">If an unexpected error occurs during processing</response>
 		[HttpPatch(ApiRoutes.Dashboard.UserRoleManagement)]
         [Authorize(Roles = nameof(Roles.Administrator))]
 		public async Task<IActionResult> AssignRole(string id, [FromBody] AssignRoleRequest request)
@@ -48,11 +48,11 @@ namespace KLab.Api.Controllers
 		/// Requires administrator role
 		/// </summary>
 		/// <param name="id">The id of the user to be deleted</param>
-		/// <response code="204">No Content: If the user is successfully deleted</response>
-		/// <response code="400">Bad Request: If the request is invalid</response>
-		/// <response code="404">Not Found: If the user with the specified id is not found</response>
-		/// <response code="422">Unprocessable Entity: If the user deletion process encounters validation errors</response>
-		/// <response code="500">Internal Server Error: If an unexpected error occurs during processing</response>
+		/// <response code="204">If the user is successfully deleted</response>
+		/// <response code="400">If the request is invalid or malformed</response>
+		/// <response code="404">If the user with the specified id is not found</response>
+		/// <response code="422">If the user deletion process encounters validation errors</response>
+		/// <response code="500">If an unexpected error occurs during processing</response>
 		[HttpDelete(ApiRoutes.Dashboard.UserManagement)]
 		[Authorize(Roles = nameof(Roles.Administrator))]
 		public async Task<IActionResult> DeleteUser(string id)
