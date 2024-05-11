d9f8984113ee62d38405bdc75f16ac851779ffc5
Amin Khashemi <hashemiamin666@gmail.com>
2024-05-11 18:44:30 +0300

    Added CORS policy configuration

---
+++ src/KLab.Api/Contracts/Origins.cs
@@ -0,0 +1,9 @@
+﻿namespace KLab.Api.Contracts
+{
+	public static class Origins
+	{
+		public static string Local => "Origins:Local";
+		public static string Network => "Origins:Network";
+		public static string Production => "Origins:Production";
+	}
+}
---
+++ src/KLab.Api/Infrastructure/Configurations/CorsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using KLab.Api.Contracts;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace KLab.Api.Infrastructure.Configurations
+{
+	public static class CorsConfiguration
+	{
+		public static CorsPolicyBuilder ConfigureCorsPolicy(
+			this CorsPolicyBuilder builder,
+			IConfiguration configuration)
+		{
+			return builder.WithOrigins(
+				configuration[Origins.Local]!,
+				configuration[Origins.Network]!,
+				configuration[Origins.Production]!)
+				.AllowAnyHeader()
+				.AllowAnyMethod()
+				.AllowCredentials();
+		}
+	}
+}
