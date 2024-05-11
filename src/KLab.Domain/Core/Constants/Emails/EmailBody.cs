dd412cb11a081a9cfdf114565a4da003f6a3079e
Amin Khashemi <hashemiamin666@gmail.com>
2024-05-11 18:41:37 +0300

    Updated email body formatting

--- src/KLab.Domain/Core/Constants/Emails/EmailBody.cs
+++ src/KLab.Domain/Core/Constants/Emails/EmailBody.cs
@@ -14,13 +14,74 @@
 		public static string Verification(string username, string verificationCode)
 		{
 			return @$"
-                <p>Dear {username},</p>
-                <p>Welcome to KLab! This email serves as confirmation that the first part of the registration has been successfully completed.</p>
-                <p>Enter this verification code to complete the registration of your account: {verificationCode}.</p>
-                <p>If you have not registered, please disregard this message.</p>
-                <br/>
-                <p>Best regards,</p>
-                <p>KLab Team</p>";
+<!DOCTYPE html>
+<html lang=""en"">
+<head>
+    <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <title>Повідомлення про реєстрацію</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }}
+        .wrapper {{
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border: 1px solid rgba(0, 0, 0, 0.2);
+        }}
+        h1 {{
+            color: #8eaccd;
+            text-align: center;
+            margin-bottom: 20px;
+        }}
+        p {{
+            color: #333333;
+            margin-bottom: 20px;
+            line-height: 1.6;
+            text-align: center;
+            font-size: 17px;
+        }}
+        .verification-code {{
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+            background-color: #f9f3cc; 
+            color: #000000;
+            padding: 20px;
+            border-radius: 10px;
+            margin-bottom: 30px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }}
+        .footer {{
+            text-align: center;
+            margin-top: 20px;
+            color: #8eaccd;
+            font-weight: bold;
+        }}
+    </style>
+</head>
+<body>
+    <div class=""wrapper"">
+        <h1>Завершення реєстрації</h1>
+        <p>Ласкаво просимо до нашого сервісу, {username}!</p>
+        <p>Ваш обліковий запис успішно зареєстровано.</p>
+        <p>Для завершення реєстрації введіть наступний код верифікації:</p>
+        <div class=""verification-code"">{verificationCode}</div>
+        <p>Якщо ви не реєструвалися, проігноруйте це повідомлення.</p>
+        <div class=""footer"">
+            <p>З найкращими побажаннями від команди КЛаб!</p>
+        </div>
+    </div>
+</body>
+</html>
+
+";
 		}
 
 		/// <summary>
@@ -32,12 +93,72 @@
 		public static string Authentication(string username, string authenticationCode)
 		{
 			return @$"
-                <p>Dear {username},</p>
-                <p>Your authentication code: {authenticationCode}.</p>
-                <p>If you did not initiate this request, please disregard this message.</p>
-                <br/>
-                <p>Best regards,</p>
-                <p>KLab Team</p>";
+<!DOCTYPE html>
+<html lang=""uk"">
+<head>
+    <meta charset=""UTF-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <title>Повідомлення про реєстрацію</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }}
+        .wrapper {{
+            border: 1px solid rgba(142, 172, 205, 0.2);
+            border-radius: 5px;
+            max-width: 600px;
+            margin: 50px auto;
+            padding: 30px;
+            box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+        }}
+        h1 {{
+            color: #8eaccd;
+            text-align: center;
+            margin-bottom: 20px;
+        }}
+        p {{
+            color: #333333;
+            margin-bottom: 20px;
+            line-height: 1.6;
+            text-align: center;
+            font-size: 17px;
+        }}
+        .authentication-code {{
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
+            background-color: #f9f3cc;
+            color: #000000;
+            padding: 20px;
+            border-radius: 10px;
+            margin-bottom: 30px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }}
+        .footer {{
+            text-align: center;
+            margin-top: 20px;
+            color: #8eaccd;
+            font-weight: bold;
+        }}
+    </style>
+</head>
+<body>
+    <div class=""wrapper"">
+        <h1>Автентифікація</h1>
+        <p>Вітаємо, {username}!</p>
+        <p>Ваш код автентифікації:</p>
+        <div class=""authentication-code"">{authenticationCode}.</div>
+        <p>Якщо ви не планували автентифікуватися, проігноруйте це повідомлення.</p>
+        <div class=""footer"">
+            <p>З найкращими побажаннями від команди КЛаб!</p>
+        </div>
+    </div>
+</body>
+</html>
+";
 		}
 	}
 }
