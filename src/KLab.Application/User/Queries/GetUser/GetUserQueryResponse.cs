3b2d2f512f0775beef0f77bc5a68923134bcf7a1
Amin Khashemi <hashemiamin666@gmail.com>
2024-05-11 20:23:38 +0300

    Added description and role to user information for client and changed query to database

--- src/KLab.Application/User/Queries/GetUser/GetUserQueryResponse.cs
+++ src/KLab.Application/User/Queries/GetUser/GetUserQueryResponse.cs
@@ -5,23 +5,30 @@
 	/// </summary>
 	public class GetUserQueryResponse
 	{
-		public string? Id { get; init; }
-		public string? Username { get; init; }
-		public string? Nickname { get; init; }
-		public string? Email { get; init; }
+		public string Id { get; init; }
+		public string Username { get; init; }
+		public string Nickname { get; init; }
+		public string Email { get; init; }
+		public string Description { get; init; }
+		public string Role { get; init; }
 		public DateTimeOffset? CreatedAtUtc { get; init; }
 
 		public GetUserQueryResponse(
-			string id, 
-			string username, 
-			string nickname, 
-			string email, 
-			DateTimeOffset? createdAtUtc)
+			string id,
+			string username,
+			string nickname,
+			string email,
+			string description,
+			string role,
+			DateTimeOffset? createdAtUtc
+			)
 		{
 			Id = id;
 			Username = username;
 			Nickname = nickname;
 			Email = email;
+			Description = description;
+			Role = role;
 			CreatedAtUtc = createdAtUtc;
 		}
 	}
